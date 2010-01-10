54301734e7b8c90406156c3e39194d25d4f53995
Paweł Biernat <pawel.biernat@gmail.com>
2010-01-11 00:53:59 +0100

    added snapshot trigger

--- snapshot_to_png.sh
+++ snapshot_to_png.sh
@@ -5,27 +5,36 @@ options1="w l lt 1 " # lt 1 pt 1"
 options2="w p lt 2 pt 2"
 
 name="*.dat"
-path="log/snapshot2/"
-files=$(find $path -name "$name" |sort| awk 'NR % 10 == 0')
+path="log/snapshot/"
+files=$(find $path -name "$name" |sort| awk 'NR % 1 == 0')
+DT=.001
+T_LAST=0.
 
-rm movie/*.png
+mkdir -p movie
+rm -f movie/*.png
 
 for f in $files; do
-    echo "" > plotter.gp	# clear the temporary file
-    nmb=$(grep -v \# $f| wc -l)	# determine the number of lines
-    nmb=$((nmb-2))		# before the last data point
-    cmd="plot"			# initialize cmd
-    t=${f:19:17}		# determine time from file name
-    echo "t = $t"		# some output for user
-    output="movie/m_$t.png"	# output file name
-    title="bubbling off for harmonic maps\\nt=$t" # graph title
-    cmd="set title \"$title\"; $cmd"		  # set the title
-    cmd="set output \"$output\"; $cmd"		  # set the output file
-    cmd="$cmd \"$f\" u (log(tan(\$1/2.))):(\$2/sin(\$1)) every ::1::$nmb w lp" # the plot
-    echo "$cmd" > plotter.gp	# echo the command to the temporary file
-    ./snapshot_to_png_template.gp # execute the template (it loads the temporary file)
+    t=${f:18:17}		# determine time from file name
+
+    TRIGGER=$(echo "scale=20;$t>$T_LAST"|bc)
+    if [ $TRIGGER = "1" ]; then
+	T_LAST=$(echo "scale=20;$T_LAST+$DT"|bc)
+	nmb=$(grep -v \# $f| wc -l)	# determine the number of lines
+	nmb=$((nmb-2))		# before the last data point
+	cmd="plot"			# initialize cmd
+	echo "" > plotter.gp	# clear the temporary file
+	output="movie/m_$t.png"	# output file name
+	title="bubbling off for harmonic maps\\nt=$t" # graph title
+	cmd="set title \"$title\"; $cmd"	      # set the title
+	cmd="set output \"$output\"; $cmd" # set the output file
+        # cmd="$cmd \"$f\" u (log(tan(\$1/2.))):(\$2/sin(\$1)) every ::1::$nmb w lp" # the plot
+	cmd="$cmd \"$f\" u 1:(\$2/sin(\$1)) every ::1::$nmb w l" # the plot
+	echo "t = $t"		 # some output for user
+	echo "$cmd" > plotter.gp # echo the command to the temporary file
+	./snapshot_to_png_template.gp # execute the template (it loads the temporary file)
+    fi
 done
 
 ./make_movie.sh
-rm movie/*.png
+# rm movie/*.png
 gmplayer movie.avi
