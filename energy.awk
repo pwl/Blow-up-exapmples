831e4601fc5f4c25e001efb901dc5143166a4a79
Paweł Biernat <pawel.biernat@gmail.com>
2010-02-25 20:51:28 +0100

    Modified energy scripts in order to calculate proper energy dependence on dimension and L

--- energy.awk
+++ energy.awk
@@ -3,6 +3,7 @@
 BEGIN {
     b=0;
     empty=0;
+    # print L, " ", N
 }
 
 # ignore comment lines and empty lines
@@ -25,7 +26,7 @@ empty == 0 && b == block {
     y=$1
     f=$2
     fy=$3
-    w=exp(-y*y/4)*y*y*(fy*fy/2 + sin(f)*sin(f)/y/y)
+    w=exp(-y*y/4)*y^(N-1)*(fy*fy/2 + L*(L+N-2)/2*sin(f)*sin(f)/y/y)
     printf "%.15G %.15G\n", y, w
     empty=0
 }
--- energy.sh
+++ energy.sh
@@ -6,9 +6,13 @@ dy=0.00001
 blocks=$(./block.awk $file)
 
 echo "$file"
+k=$(echo "$file" |sed -e 's|.*_k\([.0-9][.0-9]*\).*|\1|')
+l=$(echo "$file" |sed -e 's|.*_l\([.0-9][.0-9]*\)\.dat|\1|')
+
+# echo "$k $l"
 
 for b in $(seq 1 $blocks ); do
-    sum=$(nawk -f energy.awk "block=$((b-1))" $file \
+    sum=$(nawk -v "block=$((b-1))" -v "N=$k" -v "L=$l" -f energy.awk  $file \
 	| spline -t 0.0 8.0 $dy -P15 2> /dev/null\
 	| awk -f sum.awk )
     E=$(echo "$sum*$dy"|bc)
