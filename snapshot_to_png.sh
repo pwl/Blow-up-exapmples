1385e82111bcfccb97e6ef30dfe0aae78960888a
Paweł Biernat <pawel.biernat@gmail.com>
2010-11-09 15:10:10 +0100

    snapshot_to_png now via SVG

--- snapshot_to_png.sh
+++ snapshot_to_png.sh
@@ -7,7 +7,7 @@ options2="w p lt 2 pt 2"
 name="*.dat"
 path="log/snapshot/"
 # files=$(find $path -name "$name" |sort| head -n 10)
-files=$(find $path -name "$name" |sort -t'_' -k2n| awk 'NR % 1 == 0')
+files=$(find $path -name "$name" |sort -t'_' -k2n| awk 'NR % 20 == 0')
 DT=.001
 T_LAST=0.
 
@@ -20,12 +20,12 @@ i=1;
 
 for f in $files; do
     # t=$(awk '/t = /{print $4; exit}' $f)
-    t=$(awk '/t = /{printf("%.3f",$4); exit}' $f)
+    t=$(awk '/t = /{printf("%.10f",$4); exit}' $f)
     g=$(awk '/g = /{print $4; exit}' $f)
     s=$(awk '/s = /{printf("%.0f",$4); exit}' $f)
-    timer=$t
+    timer=$s
 
-    TRIGGER=$(echo "scale=20;$timer>=$T_LAST"|bc)
+    # TRIGGER=$(echo "scale=20;$timer>=$T_LAST"|bc)
     # echo $T_LAST $DT $t
     # echo "scale=20;$t>$T_LAST"|bc
     if [ $TRIGGER = 1 ]; then
@@ -43,8 +43,8 @@ for f in $files; do
         # cmd=" $cmd" # set the output file
         # cmd="$cmd \"$f\" u (log(tan(\$1/2.))):(\$2/sin(\$1)) every ::1::$nmb w lp" # the plot
     	# cmd="$cmd \"$f\" u ((1.+.3*sin(\$1))*sin(\$2)):((1.+.3*sin(\$1))*cos(\$2)) t\"\" w l lw 2, sin(t),cos(t)"
-    	cmd="$cmd \"$f\" u ((1.+.5*sin(\$1))*sin(\$2)):((1.+.5*sin(\$1))*cos(\$2)) t\"\" w l lw 3, sin(t),cos(t)"
-    	# cmd="$cmd \"$f\" u 1:(\$2/\$1) w lp" # the plot
+    	# cmd="$cmd \"$f\" u ((1.+.5*sin(\$1))*sin(\$2)):((1.+.5*sin(\$1))*cos(\$2)) t\"\" w l lw 3, sin(t),cos(t)"
+    	cmd="$cmd \"$f\" w l lw 2" # the plot
     	echo "t = $t"		 # some output for user
     	echo "$cmd" > plotter.gp # echo the command to the temporary file
     	./snapshot_to_png_template.gp # execute the template (it loads the temporary file)
--- snapshot_to_png_template.gp
+++ snapshot_to_png_template.gp
@@ -1,21 +1,27 @@
 #!/usr/bin/gnuplot -persist
 
-# set xlabel "x"
-# set ylabel "u(x)"
-# set xrange [1.e-8:pi]
-# set yrange [0:pi]
-set parametric;
-set xrange [ -2:2 ];
-set yrange [ -2:2 ];
-set trange [ 0:2*pi ];
-set size square;
+set xlabel "r"
+set ylabel "u(t,r)"
+set xrange [1.e-6:pi]
+set yrange [0:pi]
+set ytics (0, "{/Symbol p}/2" pi/2, "{/Symbol p}" pi, "{/Symbol p}/2+b_1" pi/2+0.573141133043885)
+set grid ytics
+set logscale x 10
+
+# set xrange [ -2:2 ];
+# set yrange [ -2:2 ];
+# set trange [ 0:2*pi ];
+# set size square;
 set nokey
-set noxtic; set noytic;
-unset border;
-unset grid;
+# set noxtic; set noytic;
+# unset border;
+# unset grid;
+
+set object 1 rect from screen 0, 0, 0 to screen 1, 1, 0 behind
+set object 1 rect fc  rgb "white"  fillstyle solid 1.0
 
 # set logscale x 10
-set term svg dynamic# size 800,600
+set term svg dynamic enhanced# size 800,600
 set key off
 # set ytics ("0" 0, "pi" pi, "2pi" 2*pi, "3pi" 3*pi, "4pi" 4*pi)
 # set ytics ("0" 0, "pi" pi, "2pi" 2*pi)
