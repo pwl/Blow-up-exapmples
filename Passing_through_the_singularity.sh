f791c96bc787bc5d4d1ae78c321486ba0e70ea23
Paweł Biernat <pawel.biernat@gmail.com>
2010-11-17 11:02:45 +0100

    Passing through the singularity plot

---
+++ Passing_through_the_singularity.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+mplotx=
+snapshot_dir="log/snapshot/"
+logfile="log/info_1/log000.dat"
+snapshot_name="*.dat"
+mrows=5
+mcols=5
+mtot=$((mcols*mrows))
+mspace=$((mtot))
+snapshot_files=$(find $snapshot_dir -name "$snapshot_name" |
+    sort -n -t'_' -k2 | awk 'NR % 70 == 0 && NR >= 0' | head -n$mtot|awk 'NR%1==0 {print}')
+blowup_file1="harvester_data_shrinker/shrinker_k3.00000_l1.0.dat"
+# blowup_file2="harvester_data_shrinker/eigen_k3.00000_l1.0_i1.dat"
+blowup_file2="harvester_data_expander/expander_k3.00000_l1.0.dat"
+startx=0.1
+starty=0.1
+stopx=0.9
+stopy=0.9
+xrange="50"
+yrange="1.2*pi"
+size_mult=1.
+T=0.328075426868397
+
+echo "" > plotter.gp
+echo "set terminal postscript enhanced color size 7,7" >> plotter.gp
+echo "set output \"graphics/snapshot_to_multiplot.ps\"" >> plotter.gp
+# echo "set size $size_mult,$size_mult" >> plotter.gp
+echo "set multiplot title \"Passing through the blow-up\"" >> plotter.gp
+
+i=0
+
+for snap in $snapshot_files; do
+
+    t=$(awk '/t = /{print $4}' $snap)
+    g=$(awk '/g = /{print $4}' $snap)
+    gprint=$(awk '/g = /{printf("%1.2E\n",$4)}' $snap)
+    s=$(awk '/s = /{printf("%.0f",$4)}' $snap)
+    du=$(awk '/du = /{printf("%.0f",$4)}' $snap)
+    T_t=$(awk "BEGIN {printf(\"%.2E\n\",$t-$T);exit 0}")
+
+    # echo "set logscale y 10" >> plotter.gp
+    echo "set key off" >> plotter.gp
+    # switch margins off
+    echo "set rmargin 0" >> plotter.gp
+    echo "set lmargin 0" >> plotter.gp
+    echo "set tmargin 0" >> plotter.gp
+    echo "set bmargin 0" >> plotter.gp
+    # setup plot size and position
+    sizex=$(echo "scale=20;$size_mult*($stopx-$startx)/$mrows"|bc)
+    sizey=$(echo "scale=20;$size_mult*($stopy-$starty)/$mcols"|bc)
+    echo "set size $sizex,$sizey" >> plotter.gp
+    orx=$(echo "a=($i%$mrows);scale=20;$startx+a*$sizex"|bc)
+    ory=$(echo "a=($i/$mrows);scale=20;$stopy-(a+1)*$sizey"|bc)
+    echo "set origin $orx,$ory" >> plotter.gp
+
+    # setup the tics for picture in the lower left corner
+    # echo "unset format" >> plotter.gp
+    echo "unset tics" >> plotter.gp
+    # echo "set grid" >> plotter.gp
+    tics=$(echo "($i%$mrows==0) && (($i/$mcols)+1==$mrows)"|bc)
+    echo "unset xlabel" >> plotter.gp
+    echo "unset ylabel" >> plotter.gp
+    if [ $tics -eq 1 ]; then
+    	echo "set tics nomirror in" >> plotter.gp
+	echo 'set ytics (0, "{/Symbol p}/2" pi/2, "{/Symbol p}" pi, "{/Symbol p}/2+b_1" pi/2+0.573141133043885)' >> plotter.gp
+	echo "set xlabel \"y\"" >> plotter.gp
+	echo "set ylabel \"|u(r,t)-f_0(y)|/N\" rotate offset screen .1*$sizex, 0." >> plotter.gp
+    fi
+
+    echo "set title \"t-T=$T_t\" offset screen .05*$sizex, screen -1.1*$sizey font \"Times-Roman,10\"" >> plotter.gp
+    echo -ne "plot [:10] [ 0:pi ] \"$snap\" u (\$1/sqrt(abs($T-$t))):(\$2/\$1) w l lw 2," >> plotter.gp
+    echo -ne "\"$blowup_file1\" index 0 w l lt 2 t \"\"," >> plotter.gp
+    echo -ne "\"$blowup_file2\" u 1:(pi-\$2) index 0 w l lt 2 t \"\"\n" >> plotter.gp
+
+    i=$((i+1))
+done
+
+echo "unset multiplot" >> plotter.gp
+
+./snapshot_to_multiplot.gp
+ps2pdf graphics/snapshot_to_multiplot.ps graphics/snapshot_to_multiplot.pdf
+evince graphics/snapshot_to_multiplot.ps
